8c2dc504714bea7e2e0a8c1220bd65d8b0112919
Victor notebook <Victor-alvess10@hotmail.com>
2025-08-23 15:19:33 -0300

    Atualizando modelo de dados do Livro

--- Models/Livro.cs
+++ Models/Livro.cs
@@ -2,4 +2,13 @@
 
 public class Livro
 {
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Genero { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
+
 }
+
+
