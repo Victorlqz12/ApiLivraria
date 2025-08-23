e6f7375d307e225d533b892a2a1de8783f6a8f0f
Victor notebook <Victor-alvess10@hotmail.com>
2025-08-23 15:57:06 -0300

    Adição de Livros, Get ver todos os livros, Get buscar livro por id

--- Controllers/LivrosController.cs
+++ Controllers/LivrosController.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiLivraria.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiLivraria.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class LivrosController : ControllerBase
 {
+    private static List<Livro> livros = new List<Livro>
+    {
+        new Livro {Id = 1, Titulo = "O Senhor dos Anéis", Autor = "J.R.R. Tolkien", Genero = "Fantasia", Preco = 59.90m, Estoque = 10 },
+        new Livro {Id = 2, Titulo = "Harry Potter e a Pedra Filosofal", Autor = "J.K. Rowling", Genero = "Fantasia", Preco = 39.90m, Estoque = 15 },
+        new Livro {Id = 3, Titulo = "O Problema dos Três Corpos", Autor = "Liu Cixin", Genero = "Ficção Científica", Preco = 49.90m, Estoque = 8 }
+
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Livro>> Get()
+    {
+        return Ok(livros);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Livro> Get(int id)
+    {
+        var livro = livros.FirstOrDefault(l => l.Id == id);
+        if (livro == null)
+        {
+            return NotFound();
+        }
+        return Ok(livro);
+    }
 }
